09187fe2ed865fe7093dbced96863a95e95d2a08
mdashfaque-mamdu-au17 <ashfaquemamdu666@gmail.com>
2022-09-09 04:23:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h2 className="bg-cyan-300">Project started here</h2>
+    </div>
+  );
+}
+
+export default App;
