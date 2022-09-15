79a6bbee50853a7f6c5ed4bc1266f7242fa890d4
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-09-16 02:23:47 +0530

    created a reusable button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React from 'react';
+import classNames from 'classnames';
 
 const Button = ({ title, ...props }) => {
-  return <button {...props}>{title}</button>;
+  return (
+    <button
+      className={classNames(
+        'w-[327px] h-[53px] text-center rounded-lg text-lg font-medium leading-6 text-white bg-deep-violet-300 lg:w-[381px]'
+      )}
+      {...props}
+    >
+      {title}
+    </button>
+  );
 };
 
 export default Button;
