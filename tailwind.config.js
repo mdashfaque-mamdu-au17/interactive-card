a46938002b83183175b122d517ec03fd21feff4b
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-09-16 02:22:30 +0530

    color changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      colors: {
+        'theme-grey-200': '#DFDEE0',
+        'deep-violet-300': '#21092F',
+        'theme-red-800': '#FF5050',
+      },
       fontFamily: {
-        spaceGrotesk: ["Space Grotesk", "sans-serif"],
+        spaceGrotesk: ['Space Grotesk', 'sans-serif'],
       },
     },
   },
