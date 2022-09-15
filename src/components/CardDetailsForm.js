655fcc91a608a4909c6da914ae3d6701f5dbec51
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-09-16 02:24:14 +0530

    validation for forms completed

--- src/components/CardDetailsForm.js
+++ src/components/CardDetailsForm.js
@@ -1,35 +1,74 @@
-import React from "react";
-import { Formik, Form } from "formik";
-import * as Yup from "yup";
-import InputField from "./InputField";
-import Button from "./Button";
+import React from 'react';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import valid from 'card-validator';
+
+import InputField from './InputField';
+import Button from './Button';
+import classNames from 'classnames';
 const CardDetailsForm = () => {
+  const formatAndSetCcNumber = (e) => {
+    const inputVal = e.target.value.replace(/ /g, '');
+    let inputNumbersOnly = inputVal.replace(/\D/g, '');
+
+    if (inputNumbersOnly.length > 16) {
+      inputNumbersOnly = inputNumbersOnly.substr(0, 16);
+    }
+
+    const splits = inputNumbersOnly.match(/.{1,4}/g);
+
+    let spacedNumber = '';
+    if (splits) {
+      spacedNumber = splits.join(' ');
+    }
+
+    return spacedNumber;
+  };
   return (
     <Formik
       initialValues={{
-        fullName: "",
-        cardNumber: "",
-        month: "",
-        year: "",
-        cvv: "",
+        firstName: '',
+        cardNumber: '',
+        month: '',
+        year: '',
+        cvv: '',
       }}
       validationSchema={Yup.object({
         firstName: Yup.string()
-          .max(15, "Must be 15 Characters or less")
-          .required("something wrong"),
+          .required("Can't be blank")
+          .test(
+            'test-number',
+            'Name is invalid',
+            (value) => valid.cardholderName(value).isValid
+          ),
         cardNumber: Yup.string()
-          .min(16, "Should be Atleast 16 digits")
-          .max(16, "Should be 16 digits only")
-          .required("Can't be blank"),
-        month: Yup.string().min(2).max(2).required("Can't be blank"),
+          .required('Card number is required')
+          .test(
+            'test-number',
+            'Card number is invalid',
+            (value) => valid.number(value?.trim()).isValid
+          ),
+        month: Yup.string()
+          .required("Can't be blank")
+          .test(
+            'test-number',
+            'Invalid month',
+            (value) => valid.expirationMonth(value).isValid
+          ),
         year: Yup.string()
-          .min(4, "Year is Invalid")
-          .max("Year is Invalid")
-          .required("Required"),
+          .required("Can't be blank")
+          .test(
+            'test-number',
+            'Invalid Year',
+            (value) => valid.expirationYear(value).isValid
+          ),
         cvv: Yup.string()
-          .min(3, "Invalid CVV")
-          .max(3, "Invalid CVV")
-          .required("Can't be blank"),
+          .required("Can't be blank")
+          .test(
+            'test-number',
+            'CVV is invalid',
+            (value) => valid.cvv(value).isValid
+          ),
       })}
       validateOnChange={false}
       validateOnBlur={false}
@@ -41,15 +80,103 @@ const CardDetailsForm = () => {
         }, 400);
       }}
     >
-      <Form>
-        <InputField
-          label="cardholder name"
-          name="firstName"
-          type="text"
-          placeholder="e.g. Jane Appleased"
-        />
-        <Button title="Confirm" type="submit" />
-      </Form>
+      {(props) => {
+        return (
+          <Form>
+            <div className="mb-5 lg:mb-[26px]">
+              <InputField
+                label="cardholder name"
+                name="firstName"
+                type="text"
+                placeholder="e.g. Jane Appleased"
+                fieldType="primary"
+                value={props.values.firstName}
+                onChange={(e) =>
+                  props.setFieldValue('firstName', e.target.value)
+                }
+              />
+            </div>
+
+            <div className="mb-2.5 lg:mb-[26px]">
+              <InputField
+                label="card number"
+                name="cardNumber"
+                type="text"
+                placeholder="e.g 1234 5678 9123 0000"
+                fieldType="primary"
+                value={props.values.cardNumber}
+                onChange={(e) => {
+                  props.setFieldValue(
+                    'cardNumber',
+                    formatAndSetCcNumber(e)
+                  );
+                }}
+              />
+            </div>
+
+            <div className="flex w-[327px] gap-[9px] lg:gap-5 mb-[35px] lg:mb-10">
+              <div className="flex flex-col">
+                <label
+                  htmlFor="month"
+                  className={classNames(
+                    'text-sm font-medium leading-4 uppercase block pb-2 w-full tracking-[1px] lg:tracking-[1px]'
+                  )}
+                >
+                  exp. date (mm/yy)
+                </label>
+                <div className="flex gap-2 lg:ga-2.5">
+                  <InputField
+                    label=""
+                    name="month"
+                    type="text"
+                    placeholder="MM"
+                    fieldType="secondary"
+                    maxLength="2"
+                    value={props.values.month}
+                    onChange={(e) => {
+                      props.setFieldValue('month', e.target.value);
+                    }}
+                  />
+
+                  <InputField
+                    label=""
+                    name="year"
+                    type="text"
+                    placeholder="YY"
+                    fieldType="secondary"
+                    maxLength="2"
+                    value={props.values.year}
+                    onChange={(e) => {
+                      props.setFieldValue('year', e.target.value);
+                    }}
+                  />
+                </div>
+                {props.errors.month || props.errors.year ? (
+                  <span className="pt-2 text-sm font-semibold leading-4 text-theme-red-800">
+                    {props.errors.month || props.errors.year}
+                  </span>
+                ) : null}
+              </div>
+              <div>
+                <InputField
+                  label="cvc"
+                  name="cvv"
+                  type="text"
+                  placeholder="e.g 123"
+                  fieldType="thirdType"
+                  maxLength="3"
+                  value={props.values.cvv}
+                  onChange={(e) => {
+                    props.setFieldValue('cvv', e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+
+            <Button title="Confirm" type="submit" />
+          </Form>
+        );
+      }}
     </Formik>
   );
 };
--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,14 +1,52 @@
-import { useField } from "formik";
-import React from "react";
+import { useField } from 'formik';
+import React from 'react';
+import classNames from 'classnames';
 
-const InputField = ({ label, ...props }) => {
+const InputField = ({ label, fieldType, ...props }) => {
   const [field, meta] = useField(props);
-  console.log(meta.error);
+
+  const applyStyle = () => {
+    if (fieldType === 'primary') {
+      return 'w-[327px] h-11 lg:w-[381px]';
+    }
+    if (fieldType === 'secondary') {
+      return 'w-[72px] lg:w-20 h-11';
+    }
+
+    if (fieldType === 'thirdType') {
+      return 'w-[164px] lg:w-[191px] h-11';
+    }
+  };
   return (
     <>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <input {...field} {...props} />
-      {meta.error ? <div className="error">{meta.error}</div> : null}
+      {fieldType === 'primary' || fieldType === 'thirdType' ? (
+        <label
+          htmlFor={props.id || props.name}
+          className={classNames(
+            'text-xs font-medium leading-4 uppercase tracking-[2px] block pb-2'
+          )}
+        >
+          {label}
+        </label>
+      ) : null}
+      <div>
+        <input
+          {...field}
+          {...props}
+          className={classNames(
+            'block transition ease-in-out bg-white border rounded-lg form-control  focus:outline-none focus:rounded-lg placeholder:text-deep-violet-300 placeholder:text-opacity-25 placeholder:font-medium placeholder:leading-6 placeholder:text-lg pl-4',
+            applyStyle(),
+            meta.error
+              ? 'border-theme-red-800'
+              : 'border-theme-grey-200'
+          )}
+        />
+        {fieldType !== 'secondary' && meta.error ? (
+          <span className="block mt-2 text-sm font-semibold leading-4 text-theme-red-800">
+            {meta.error}
+          </span>
+        ) : null}
+      </div>
     </>
   );
 };
