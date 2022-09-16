0368ae054bbc3e024d4bc7a0ba3fa389caa9d932
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-09-17 01:23:33 +0530

    thank you page and card details form is shown conditionally and added all the states

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 import CardDetailsForm from './components/CardDetailsForm';
+import CardAnimate from './components/CardAnimate';
+import { useState } from 'react';
+import ThankYou from './components/ThankYou';
+import { useEffect } from 'react';
 
 function App() {
+  const [cardDetails, setCardDetails] = useState({
+    firstName: '',
+    cardNumber: '',
+    month: '',
+    year: '',
+    cvv: '',
+  });
+  const [showThankYou, setShowThankYou] = useState(false);
+
+  useEffect(() => {
+    if (!showThankYou) {
+      setCardDetails({
+        firstName: '',
+        cardNumber: '',
+        month: '',
+        year: '',
+        cvv: '',
+      });
+    }
+  }, [showThankYou]);
   return (
     <div className="font-spaceGrotesk">
-      <div>
-        <div className="flex justify-center px-6">
-          <CardDetailsForm />
+      <div className="lg:grid lg:grid-cols-2">
+        <div>
+          <CardAnimate cardDetails={cardDetails} />
         </div>
+
+        {showThankYou ? (
+          <div className="px-6 mt-[91px] flex justify-center">
+            <ThankYou setShowThankYou={setShowThankYou} />
+          </div>
+        ) : (
+          <div className="flex justify-center px-6 mt-[91px]">
+            <CardDetailsForm
+              setCardDetails={setCardDetails}
+              cardDetails={cardDetails}
+              setShowThankYou={setShowThankYou}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
