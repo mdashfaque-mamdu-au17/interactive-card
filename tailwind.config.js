0b2e48b81fa1f06fa7aea405427e564ea7d89fda
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-09-17 01:22:05 +0530

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         'theme-grey-200': '#DFDEE0',
         'deep-violet-300': '#21092F',
         'theme-red-800': '#FF5050',
+        'polish-grey': '#8F8694',
       },
       fontFamily: {
         spaceGrotesk: ['Space Grotesk', 'sans-serif'],
